207dc309a98a987876ee56ddc70fc10c83273a7a
zhufengzhufeng <894918097@qq.com>
2016-11-26 11:36:33 +0800

    concat

--- 5.buffer/3.buffer的方法.js
+++ 5.buffer/3.buffer的方法.js
@@ -19,7 +19,21 @@ var buf2 = new Buffer('峰培训');
 // targetStart, 目标的开始
 // sourceStart, 源的开始
 // sourceEnd 源的结束
-buf1.copy(buffer,0);
+buf1.copy(buffer);
 buf2.copy(buffer,3);
 console.log(buffer.toString()); //像buffer中写入内容时要注意偏移量的设置
 //concat连接多个buffer
+var buf1 = new Buffer('珠');
+var buf2 = new Buffer('峰1培训');
+//list, 要连接的列表
+//totalLength 总长度
+console.log(Buffer.concat([buf1,buf2],3000).toString());
+//写一个concat方法：
+//1.可以不写长度，全部拷贝到一个中
+//2.写长度过短，写不进去就不要了
+//3.长度过长 如果过长就不要过长的内容，只要有效内容(截取有效长度)
+Buffer.myConcat = function (list,totalLength) {
+
+    return
+};
+console.log(Buffer.myConcat([buf1,buf2],3000).toString());
