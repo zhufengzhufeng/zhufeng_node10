9cb65a4ae1d508a0888f423a30b390c9ff62208e
zhufengzhufeng <894918097@qq.com>
2016-11-26 18:39:49 +0800

    事件

--- 8.events/1.events.js
+++ 8.events/1.events.js
@@ -16,12 +16,16 @@ Man.prototype.on = function (eventName,callback) {
 };
 Man.prototype.emit = function (eventName) {
     //截取除了事件名的第一项其他的组成一个数组，传递给item执行
+    //var args = [].slice.call(arguments,1);
     var args =Array.from(arguments).slice(1);
     if(this.events[eventName]){
         //箭头函数，它里面没有this指向指向的就是上一级的this
+      /*  this.events[eventName].forEach(function(item){
+            item.apply(null,args);
+        },this); */
         this.events[eventName].forEach((item)=>{
             item.apply(null,args);
-        },this); //改变this指向
+        }); //改变this指向
     }
 };
 function buyPack(who,who1) {console.log('有钱了买包'+'给'+who+who1+'知道了');}
@@ -30,3 +34,4 @@ var man = new Man();
 man.on('有钱了',buyPack);
 man.on('有钱了',buyCar);
 man.emit('有钱了','妹子','女朋友');
+
