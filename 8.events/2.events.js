5ac25f65841af4c404a09b8e5a83c47f9c71f8d0
zhufengzhufeng <894918097@qq.com>
2016-11-27 10:57:57 +0800

    removeListener

--- 8.events/2.events.js
+++ 8.events/2.events.js
@@ -19,17 +19,20 @@ Man.prototype.emit = function (eventName) {
 //移除事件监听的方法
 Man.prototype.removeListener = function (eventName,callback) {
     this._events[eventName] = this._events[eventName].filter(function (item) {
-        return item!=callback; //在数组中过滤掉相同的callback
+        //返回false是删掉，有一个fasle就是false，既要数组中不包含callback，还有one上也不含callback
+        return (item!=callback)&&(callback!=item.g); //在数组中过滤掉相同的callback
     });
 };
 //once方法，把函数绑定一次，当函数执行后，删除掉自己，在触发emit就无法找到对应的方法执行了
 Man.prototype.once = function (eventName,callback) {
-    //先绑定，在方法执行后在移除掉
+    //先绑定，在方法执行后在移除掉  {有钱:[one]}
     var that = this;
     function one() {
         callback.apply(this,arguments);//触发原有的callback函数
         that.removeListener(eventName,one); //删除掉原有的one函数
     }
+    //在one中可以找到callback
+    one.g = callback;
     this.on(eventName,one); //在emit时触发的是one函数
 };
 //移除的方法 找到对象{"有钱":[callback]}
@@ -44,7 +47,5 @@ function buyPack(who) {
 man.on('有钱',buyCar);
 man.once('有钱',buyPack);
 man.removeListener('有钱',buyCar);
-man.emit('有钱','妹子');
-man.emit('有钱','妹子');
-man.emit('有钱','妹子');
+man.removeListener('有钱',buyPack);
 man.emit('有钱','妹子');
